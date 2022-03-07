95e19baa02c294a9e207405d53da0ff6e7f00887
prokash Pul <pprokash102@gmail.com>
2022-03-07 11:11:45 +0600

    show data dispaly

--- js/app.js
+++ js/app.js
@@ -1,32 +1,39 @@
 // add item button event handel add
 const addProduct = () => {
-    const productName = document.getElementById('product-name');
-    const productNameValue = productName.value;
-    const productNumber = document.getElementById('quadientity');
-    const productNumberValue = productNumber.value;
-    if (!productNameValue || !productNumberValue) {
-        return;
+        const productName = document.getElementById('product-name');
+        const productNameValue = productName.value;
+        const productNumber = document.getElementById('quadientity');
+        const productNumberValue = productNumber.value;
+        if (!productNameValue || !productNumberValue) {
+            alert("Please Input Field can't empity")
+            return;
+        }
+        setProduct(productNameValue, productNumberValue);
+        showDataDispaly(productNameValue, productNumberValue);
+        productName.value = '';
+        productNumber.value = '';
     }
-    setProduct(productNameValue, productNumberValue)
-    productName.value = '';
-    productNumber.value = '';
+    // DISPALY SHOW
+const showDataDispaly = (name, quadientity) => {
+    const showDisplay = document.getElementById('add-items');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+                <tr>
+                <td>${name}</td>
+                <td>${quadientity}</td>
+                </tr>
+                `
+    showDisplay.appendChild(tr);
 }
-
 
 // set prodat localstor
 
 const displyResult = () => {
-    const showDisplay = document.getElementById('add-items');
+
     const product = JSON.parse(localStorage.getItem('product')) ? JSON.parse(localStorage.getItem('product')) : [];
     product.forEach(item => {
-        const tr = document.createElement('tr');
-        tr.innerHTML = `
-                <tr>
-                <td>${item.name}</td>
-                <td>${item.quadientity}</td>
-                </tr>
-                `
-        showDisplay.appendChild(tr);
+        showDataDispaly(item.name, item.quadientity)
+            // console.log(item)
     });
 
 }
@@ -35,11 +42,12 @@ displyResult()
 const setProduct = (name, quadientity) => {
     let produtRecord = new Array();
     produtRecord = JSON.parse(localStorage.getItem('product')) ? JSON.parse(localStorage.getItem('product')) : [];
-    produtRecord.push({
+    produtRecord.unshift({
         "name": name,
         "quadientity": quadientity
     })
 
     const productObj = JSON.stringify(produtRecord);
     localStorage.setItem("product", productObj);
+
 }
